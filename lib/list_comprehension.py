ece97d3f48a1b7830e1fa24040c988c9503e7551
anisamuema <anisa.muema.school@gmail.com>
2023-11-22 10:24:00 +0300

    understand lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+   num_list = [0, 1, 3, 5, 7, 8, 9]
+   evens = [x for x in num_list]
+   print (evens)
 
+   
 def make_exclamation(sentence_list):
     pass
